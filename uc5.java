dccf5fae04f02cb66cd5975bab13303421b28783
m68741530-alt <m68741530@gmail.com>
2026-03-25 14:45:22 +0530

    Create uc5.java

---
+++ uc5.java
@@ -0,0 +1,66 @@
+import java.util.*;
+
+// Reservation class
+class Reservation {
+    private String guestName;
+    private String roomType;
+
+    public Reservation(String guestName, String roomType) {
+        this.guestName = guestName;
+        this.roomType = roomType;
+    }
+
+    public String getGuestName() {
+        return guestName;
+    }
+
+    public String getRoomType() {
+        return roomType;
+    }
+}
+
+// Booking Request Queue (FIFO)
+class BookingRequestQueue {
+    private Queue<Reservation> queue;
+
+    public BookingRequestQueue() {
+        queue = new LinkedList<>();
+    }
+
+    public void addRequest(Reservation reservation) {
+        queue.offer(reservation);
+    }
+
+    public boolean hasPendingRequests() {
+        return !queue.isEmpty();
+    }
+
+    public Reservation processRequest() {
+        return queue.poll();
+    }
+}
+
+// Main class
+public class UseCase5BookingRequestQueue {
+
+    public static void main(String[] args) {
+
+        System.out.println("Booking Request Queue");
+
+        BookingRequestQueue bookingQueue = new BookingRequestQueue();
+
+        Reservation r1 = new Reservation("Abhi", "Single");
+        Reservation r2 = new Reservation("Subha", "Double");
+        Reservation r3 = new Reservation("Vanmathi", "Suite");
+
+        bookingQueue.addRequest(r1);
+        bookingQueue.addRequest(r2);
+        bookingQueue.addRequest(r3);
+
+        while (bookingQueue.hasPendingRequests()) {
+            Reservation r = bookingQueue.processRequest();
+            System.out.println("Processing booking for Guest: " 
+                + r.getGuestName() + ", Room Type: " + r.getRoomType());
+        }
+    }
+}
